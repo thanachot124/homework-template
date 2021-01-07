7364dd532aeacbd4dcf22c7e8a885cdace70d4f5
kulwadeeku <77065282+kulwadeeku@users.noreply.github.com>
2021-01-07 10:49:03 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,5 @@
 public class Main {
    public static void main(String[] args) {
-     float v1 = 0, v2 = 0;
-     float vsum = v1 + v2;
-     System.out.println("Sum of " + v1 + " + " + v2 + " = " + vsum);
-     if (vsum > 50) {
-       System.out.println("PASS");
-     } else {
-       System.out.println("Not PASS");
-     }
+      // add your code here
    }
 }
